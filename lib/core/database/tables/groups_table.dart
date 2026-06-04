f2360ba0c50ebe77cc5b364b832d849bf27bdc0e
janvi <janvi220228@gmail.com>
2026-06-04 11:20:23 +0530

    i forgot to mention that its stored as set

--- lib/core/database/tables/groups_table.dart
+++ lib/core/database/tables/groups_table.dart
@@ -6,7 +6,7 @@ const _uuid = Uuid();
 class GroupsTable extends Table {
   TextColumn get id => text().clientDefault(() => _uuid.v4())();
   TextColumn get name => text()();
-  // storing member public keys as comma separated string
+  // storing member public keys as a set (comma separated string)
   TextColumn get users => text().withDefault(const Constant(''))();
   TextColumn get description => text().nullable()();
 
